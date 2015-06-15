d9ea7666f262e2415dc60c07a65393feb39bbaf0
glaubert <glaubert.schult@gmail.com>
2015-06-15 11:53:09 -0300

    iniciando projeto

--- src/main/java/br/gms/wsvaleuboi/rest/Api.java
+++ src/main/java/br/gms/wsvaleuboi/rest/Api.java
@@ -3,7 +3,7 @@ package br.gms.wsvaleuboi.rest;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("api")
+@ApplicationPath("rest")
 public class Api extends Application {
 
 }
